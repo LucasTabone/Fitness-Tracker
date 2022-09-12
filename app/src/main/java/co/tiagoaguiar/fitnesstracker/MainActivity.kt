06bd02251564e99522471aa213052fa856eacd10
Lucas Tabone <Lucas.tabone@hotmail.com>
2022-09-12 17:11:55 -0300

    Layout and AutoCompleteText - 009

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -48,7 +48,8 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 2 -> {
-                    // abrir uma outra activity por EX:
+                    val intent = Intent(this@MainActivity, TmbActivity::class.java)
+                    startActivity(intent)
                 }
             }
             Log.i("teste", "Clicou $id !!")
